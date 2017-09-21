ab91c60ff53f49f605f20bb7c3e0d23d84206201
JSP-GIHS <JSP-GIHS@users.noreply.github.com>
2017-09-21 10:57:54 +0930

    Update Main.java
    Remove a line and stop string binding maybe?

--- Main.java
+++ Main.java
@@ -2,11 +2,10 @@ public class Main {
 
   public static void main(String[] args) {
       for (int i = 1; i < 12; i += 2) {
-          String out = "";
           for (int j = 1; j < 12; j += 2) {
-              out = out + (i * j) + " ";
+              System.out.print((i * j) + " ");
           }
-          System.out.println(out);
+          System.out.println(" ");
       }
   }
 }
